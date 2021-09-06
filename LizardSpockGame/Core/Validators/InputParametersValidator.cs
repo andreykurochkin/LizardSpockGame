8eea010641fc9226c7ae7f11ea64dbe785c91716
andreykurochkin <andrey.kurochkin@hotmail.com>
2021-09-06 16:14:12 +0300

    Add project files.

---
+++ LizardSpockGame/Core/Validators/InputParametersValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LizardSpockGame.Core.Validators {
+    /// <summary>
+    /// validates parameters
+    /// </summary>
+    public class InputParametersValidator {
+        private readonly string[] _source;
+        public InputParametersValidator(string[] source) => _source = source;
+        public (bool, string) Validate() {
+            return (true, string.Empty);
+        }
+        private bool LengthIsLessThenThree() {
+            throw new NotImplementedException();
+        }
+        private bool LengthIsEven() {
+            throw new NotImplementedException();
+        }
+        private bool ContainsRepeatedItems() {
+            throw new NotImplementedException
+        }
+    }
+}
---
+++ LizardSpockGame/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace LizardSpockGame {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
