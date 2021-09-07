35cd8e75026e72742d326626df074e15bb19ec51
andreykurochkin <andrey.kurochkin@hotmail.com>
2021-09-07 11:08:11 +0300

    initial commit

---
+++ LizardSpockGame/Core/Crypto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+namespace LizardSpockGame.Core {
+    public static class Extensions {
+        public static string ToHexString(this byte[] bytes) => BitConverter.ToString(bytes).Replace("-", "");
+    }
+    public class Key {
+        private readonly byte[] _key;
+        private HMAC _hmac;
+        public Key() {
+            _key = CreateRandomBytes();
+            _hmac = new HMACSHA256(_key);
+        }
+        public override string ToString() => _key.ToHexString();
+        public byte[] ComputeHmac(string input) => _hmac.ComputeHash(Encoding.UTF8.GetBytes(input));
+        private byte[] CreateRandomBytes() {
+            byte[] bytes = new byte[16];
+            RandomNumberGenerator.Fill(bytes);
+            return bytes;
+        }
+    }
+}
--- LizardSpockGame/Program.cs
+++ LizardSpockGame/Program.cs
@@ -1,9 +1,21 @@
-﻿using System;
-
+﻿using LizardSpockGame.Core;
+using LizardSpockGame.Core.Validators;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+//using CryptHash.Net.Hash;
 namespace LizardSpockGame {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var inputParametersValidator = new InputParametersValidator(args);
+            var validationResult = inputParametersValidator.Validate();
+            if (!validationResult.Item1) {
+                Console.WriteLine(validationResult.Item2);
+                return;
+            }
+            var key = new Key();
+            Console.Write("HMAC: ");
+            Console.Write(key.ToString());
         }
     }
 }
