d06fa5b563ba789f4df9dd6abfa4be15dc7b15b9
andreykurochkin <andrey.kurochkin@hotmail.com>
2021-09-08 20:12:16 +0300

    updated comment

--- LizardSpockGame/Core/RulesInfo.cs
+++ LizardSpockGame/Core/RulesInfo.cs
@@ -14,13 +14,13 @@ namespace LizardSpockGame.Core {
         }
         public int GetNextIndex(int currentIndex) => (currentIndex < _turns.Length - 1) ? ++currentIndex : 0;
         /// <summary>
-        /// tuple -> <key, winners: ones who win the key, loosers: ones who are weaker then the key>
+        /// tuple -> <key, winners>
+        /// from point of view of a key all items that can beat him are inside the winners
         /// </summary>
         /// <returns></returns>
         public IEnumerable<(string, string[])> GetValues() {
             var result = new List<(string, string[])>();
             for (int i = 0; i < _turns.Length; i++) {
-                // from point of view of a key all items that can beat him are inside the winners
                 var key = _turns[i];
                 var winners = new string[_amountOfWinners];
                 var nextIndex = GetNextIndex(i);
